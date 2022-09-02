c7576c6adc48aeedf6b0854d971ed2cf7ebe6a07
trim.e.david@gmail.com <56784170+Trim-D@users.noreply.github.com>
2022-09-02 11:22:31 -0800

    Added duplicate value check
    Checks for duplicate non-mega values, generating a replacement value if found

--- mega_millions_number_generator.py
+++ mega_millions_number_generator.py
@@ -1,9 +1,4 @@
 # Create a number generator for Mega Millions Lottery tickets.
-# Requirements:  
-# 5 "white" numbers valued between 1-70
-# 1 "mega ball" number valued between 1-25
-# Provide randomly generated numbers for the number of tickets submitted by user input
-# Print out tickets
 
 import random
 tickets = 0
@@ -11,15 +6,23 @@ req_tickets = int(input("How many tickets will you purchase? "))
 white_numbers = []
 mega_ball = []
 while tickets < req_tickets:
+    # generate 5 random numbers between 1-70 for each ticket
     for white_num in range(1, 5 + 1):
         num_selection = random.randint(1, 70)
-        white_numbers.append(num_selection)
+        # checks for duplicate values, re-runs generator if found, otherwise, appends to list
+        if num_selection in white_numbers:
+            num_selection = random.randint(1, 70)
+        else:   
+            white_numbers.append(num_selection)
+    # generate 1 random number between 1-25 for each ticket
     for mega_num in range(req_tickets - 1):
         mega_selection = random.randint(1,25)
         mega_ball.append(mega_selection)
     print(*white_numbers, f"({mega_ball[0]})")
+    # deletes all values in the list each cycle
     del white_numbers[:]
     del mega_ball[:]
+    # increments # of tickets each cycle until loop condition met
     tickets += 1
 
 
